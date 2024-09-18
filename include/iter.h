3fb9f7fc167af1022843dcbcb4e31b439bc74718
seohyeon <seohyeon@seohyeons-MacBook-Air.local>
2024-09-18 00:42:13 -0700

    feat: painAliquots

---
+++ include/iter.h
@@ -0,0 +1,8 @@
+#ifndef ITER_H_
+#define ITER_H_
+
+#include <stdio.h>
+
+void printAliquotsIter(int);
+
+#endif
---
+++ include/recur.h
@@ -0,0 +1,8 @@
+#ifndef RECUR_H_
+#define RECUR_H_
+
+#include <stdio.h>
+
+void printAliquotsRecur(int, int, int);
+
+#endif
