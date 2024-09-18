d7b5aa4cf235a53b6b6b41ad176a734709f84463
seohyeon <seohyeon@seohyeons-MacBook-Air.local>
2024-09-18 03:38:48 -0700

    feat: countValues

--- include/iter.h
+++ include/iter.h
@@ -2,7 +2,12 @@
 #define ITER_H_
 
 #include <stdio.h>
+#include <stdlib.h>
+
+#define INT long long int
 
 void printAliquotsIter(int);
+INT findMinimumDifferenceIter(int *, unsigned int);
+int countValuesIter(int *, unsigned int, int);
 
 #endif
